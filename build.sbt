021021203b699626ea823bb454a1e2ca689d1d38
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2019-11-17 22:14:47 +0100

    init

---
+++ build.sbt
@@ -0,0 +1,3 @@
+name := "prototype-scala-raft"
+version := "0.1"
+scalaVersion := "2.13.1"
---
+++ src/main/scala/de/maxbundscherer/scala/raft/Main.scala
@@ -0,0 +1,6 @@
+package de.maxbundscherer.scala.raft
+
+object Main extends App {
+
+  println("hello world")
+}
