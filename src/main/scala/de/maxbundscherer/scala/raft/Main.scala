2a5f4c9114e695deae01d6d057a278a39b2752aa
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2019-12-22 17:04:52 +0100

    Add different nodes

--- src/main/scala/de/maxbundscherer/scala/raft/Main.scala
+++ src/main/scala/de/maxbundscherer/scala/raft/Main.scala
@@ -9,25 +9,12 @@ object Main extends App {
 
   import de.maxbundscherer.scala.raft.services._
 
-  private implicit val actorSystem: ActorSystem = ActorSystem("system")
+  private implicit val actorSystem: ActorSystem = ActorSystem("raftSystem")
   private implicit val executionContext: ExecutionContextExecutor = actorSystem.dispatcher
   private implicit val timeout: Timeout = 15.seconds
 
   private val log = actorSystem.log
 
-  private val counterService = new CounterService()
-
-  counterService.reset().map(response =>
-    log.info(s"Got response from reset '$response'")
-  )
-
-  counterService.increment(5).map(response =>
-    log.info(s"Got response from increment '$response'")
-  )
-
-  counterService.decrement(4).map(response => {
-    log.info(s"Got response from decrement '$response'")
-    actorSystem.terminate()
-  })
+  private val raftService = new RaftService(numberNodes = 5)
 
 }
--- src/main/scala/de/maxbundscherer/scala/raft/services/RaftService.scala
+++ src/main/scala/de/maxbundscherer/scala/raft/services/RaftService.scala
@@ -5,15 +5,21 @@ import akka.actor.{ActorRef, ActorSystem}
 import akka.util.Timeout
 import scala.concurrent.Future
 
-class RaftService()(implicit actorSystem: ActorSystem, timeout: Timeout) {
+class RaftService(numberNodes: Int)(implicit actorSystem: ActorSystem, timeout: Timeout) {
 
   import de.maxbundscherer.scala.raft.actors.NodeActor
   import de.maxbundscherer.scala.raft.aggregates.RaftAggregate._
 
   /**
-    * Private actor ref (use proxy in cluster)
-    */
-  private val oneNodeActorRef: ActorRef =
-    actorSystem.actorOf(NodeActor.props, NodeActor.prefix + "0")
+   * Declare nodes
+   */
+  final val nodes: Array[ActorRef] = new Array[ActorRef](numberNodes)
+
+  /**
+   * Start nodes
+   */
+  for (i <- 0 until numberNodes)
+    nodes(i) = actorSystem.actorOf(props = NodeActor.props,
+                                   name = s"${NodeActor.prefix}-$i")
 
 }
