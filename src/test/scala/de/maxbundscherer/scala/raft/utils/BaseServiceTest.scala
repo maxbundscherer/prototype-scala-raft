2336dccee7eb2ffb642d5652153d36909d2b6f0a
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2019-12-29 18:05:07 +0100

    Add logging adapter to baseServiceTest

--- src/test/scala/de/maxbundscherer/scala/raft/utils/BaseServiceTest.scala
+++ src/test/scala/de/maxbundscherer/scala/raft/utils/BaseServiceTest.scala
@@ -1,18 +1,21 @@
 package de.maxbundscherer.scala.raft.utils
 
 import de.maxbundscherer.scala.raft.services.RaftService
+import akka.event.LoggingAdapter
 import org.scalatest.{AsyncWordSpec, Matchers}
 
 object BaseServiceTest extends Configuration {
 
   import akka.actor.ActorSystem
+  import akka.util.Timeout
   import scala.concurrent.ExecutionContextExecutor
   import scala.concurrent.duration._
-  import akka.util.Timeout
 
   private implicit val actorSystem: ActorSystem = ActorSystem("testSystem")
   private implicit val executionContext: ExecutionContextExecutor = actorSystem.dispatcher
   private implicit val timeout: Timeout = 15.seconds
+
+  private lazy val log: LoggingAdapter = actorSystem.log
 
   private lazy val raftService = new RaftService(numberNodes = Config.nodes)
 
@@ -20,6 +23,7 @@ object BaseServiceTest extends Configuration {
 
 trait BaseServiceTest extends AsyncWordSpec with Matchers {
 
+  val log: LoggingAdapter = BaseServiceTest.log
   val raftService: RaftService = BaseServiceTest.raftService
 
 }
